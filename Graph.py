a65acb40a772d3c9e42a8407ca6d733aebe744ba
mspruc <55182629+mspruc@users.noreply.github.com>
2021-04-23 14:28:10 +0200

    commit?

--- Graph.py
+++ Graph.py
@@ -4,22 +4,21 @@ import math
 import urllib.request
 import PIL
 import random as rand
-class Graph:
 
+class Graph:
     # Init creates empty graph
-    def __init__(self):                     #graph constructor
+    def __init__(self, n):                     #graph constructor
         self.fig, self.ax = plt.subplots()  #init graph components
         self.centroidToPoints = {}          #points connected to centroids
         self.lst_cluster_a = []             #cluster from 1 centroid
         self.lst_cluster_b = []
         self.edgesArray = {}                #All edges passing from a given point
+        self.n = n
+        self.color_dict = {}
 
-    #Create TSP
-    def calculate_edges(self, lst): #calculate the distance from a given point to all other points in the array.
-        for i in range(len(lst)):
-            for j in range(len(lst)):
-                edge_dist = math.sqrt(((lst[i][1][0]-lst[j][1][0])**2) + ((lst[i][1][1]-lst[j][1][1])**2))
-                #if(lst[i][0] == 1)
+        for j in range(n): #Generate colors                             
+            color = self.rgb_to_hex((rand.randint(0, 255),rand.randint(0, 255),rand.randint(0, 255)))
+            self.color_dict.update({j:color})
 
     # Creates a connection to the graph which allows us to check for mouse clicks.
     def connect_to_graph(self, typeEvent, typeUserInput):   
@@ -29,49 +28,32 @@ class Graph:
     def draw_new_store(self, event):
             self.ax.plot(event.xdata, event.ydata, 'o')
 
-    # Creates centroids
-
-    #def draw_new_centroid(self,centroid):
-    #    self.ax.plot(centroid[0], centroid[1], 'X')
-
     #takes an array of centroids and plots them on the graph
     def draw_new_centroids(self, centroids):
         for centroid in centroids:
             self.ax.plot(centroid[0], centroid[1], 'X')
 
-    #def draw_new_centroids(self, centroid1, centroid2):
-    #    self.ax.plot(centroid1[0], centroid1[1], 'X')
-    #    self.ax.plot(centroid2[0], centroid2[1], 'X')
-    
+    #removes old centroids from the graph plot.
     def remove_old(self,n):
         if len(self.ax.lines) > 5:
             for i in range(n):
                 self.ax.lines.pop()
+                print("popped")
 
     def rgb_to_hex(self,rgb):
         return '#'+'%02x%02x%02x' % rgb
 
-    def recolor(self,lst,n,amntVertices):
-        color_dict = {}
+    def divideCentroids(self):
+        for i in range(amntVertices):   
+            if lst[i][0] == key:
+                centroidToPoints.update()
 
-        for j in range(n):                                  #Generate colors
-            color = self.rgb_to_hex((rand.randint(0, 255),rand.randint(0, 255),rand.randint(0, 255)))
-            color_dict.update({j:color})
-
-        print(color_dict)
-
-        for i in range(len(self.ax.lines)):
-            
-            for key in color_dict:
-                print(key)
-                print(i)
-
+    #recolors stores according to centroids/cluster
+    def recolor(self,lst,amntVertices):
+        for i in range(amntVertices):   
+            for key in self.color_dict:
                 if lst[i][0] == key:
-                    self.ax.lines[i].set_color(color_dict[key])
-
-
-        print(self.lst_cluster_a)
-
+                    self.ax.lines[i].set_color(self.color_dict[key])
 
     def set_background(self,img):
         self.ax.imshow(img)
--- main.py
+++ main.py
@@ -2,16 +2,26 @@ from Graph import *
 from UserInput import *
 from KMeansAlgorithm import *
 from GoogleMapsConnection import *
+from dotenv import dotenv_values
+
+#load_dotenv()  # take environment variables from .env.
+
+#config = {
+#    **dotenv_values(".env.shared"),  # load shared development variables
+#    **dotenv_values(".env.secret"),  # load sensitive variables
+#    **os.environ,  # override loaded values with environment variables
+#}
+
 def main():
+    kmeans = KMeansAlgorithm(4)     #Init KMeans algoritmen
+    graph = Graph(kmeans.n)         #Init graph          
+    usein = UserInput(graph,kmeans) #Init UserInput klassen
 
-    graph = Graph()               #Init graph          
-    gapi = GoogleMapsConnection() #Init google maps api
-
+    gapi = GoogleMapsConnection()  #Init google maps api
     gapi.get_city_json('Roskilde') #Fetch the coordinates 
     img = gapi.getIMG()            #Fetch picture 
 
-    kmeans = KMeansAlgorithm(2)      #Init KMeans algoritmen
-    usein = UserInput(graph,kmeans) #Init UserInput klassen
+    
 
     graph.set_background(img) #Sætter graph baggrunden til img variablen
     graph.updateGraph()       #Opdatere og viser grafen
